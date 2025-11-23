cc12f2bec0b623cedb574b49edf941bb9de16fdb
alex <alex.damosys@gmail.com>
2025-11-23 14:28:03 +0900

    google auth login ok, but error in fist page. browser console message make unsafe-inline. it is fixed and required to deploy again

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,27 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
+              "style-src 'self' 'unsafe-inline'",
+              "img-src 'self' data: https: blob:",
+              "font-src 'self' data:",
+              "connect-src 'self' https://accounts.google.com",
+              "frame-src 'self' https://accounts.google.com",
+            ].join("; "),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
